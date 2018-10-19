bf8069803a1afa2e1e24cbbbf1c91254901d60a8
lop_ezreal <lop_adoule@hotmail.com>
2018-10-19 16:30:08 +0800

    test commit

--- im-test/src/main/java/com/ledo/im/mapper/ImSessionMapper.java
+++ im-test/src/main/java/com/ledo/im/mapper/ImSessionMapper.java
@@ -6,6 +6,6 @@ import tk.mybatis.mapper.common.Mapper;
 import com.ledo.im.model.ImSession;
 
 public interface ImSessionMapper extends Mapper<ImSession>,ConditionMapper<ImSession> {
-  //其他必须手写的接口...
+  //其他必须手写的接口...42
 
 }
